090193291e24e24424b0636ddc1fd164503045f2
renansao <32778681+renansao@users.noreply.github.com>
2020-06-23 22:39:50 -0300

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/disc/bot/pitao/PitaobotApplication.java
@@ -0,0 +1,13 @@
+package com.disc.bot.pitao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitaobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitaobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disc/bot/pitao/PitaobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disc.bot.pitao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitaobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
